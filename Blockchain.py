295c686cc84cb69bb40ab43409b87f07324f76f3
Ziad Abouelfarah <ziadabouelfarah2@gmail.com>
2018-08-16 12:27:48 +0100

    Add Block Data

--- Blockchain.py
+++ Blockchain.py
@@ -1,4 +1,5 @@
 import hashlib
+import json
 import time
 
 class Block:
@@ -6,31 +7,40 @@ class Block:
 	data = None
 	next = None
 	hash = None
-	nonce = 0
+	pow = 0
 	previous_hash = 0x0
 	time = time.time()
 
 	def __init__(self,data):
 		self.data = data
 
+	def block_data(self):
+		block = {
+			"index": str(self.blockNo),
+			"data": str(self.data),
+			"pow": str(self.pow),
+			"time": str(time.strftime("%D %H:%M", time.localtime(int(self.time)))),
+			"previous_hash": str(self.previous_hash),
+		}
+		return json.dumps(block, sort_keys=True, indent=4)
+
 	def hash(self):
-		h = hashlib.sha256()
-		h.update(
-			str(self.time).encode('utf-8')+
-			str(self.previous_hash).encode('utf-8')+
-			str(self.nonce).encode('utf-8')+
-			str(self.data).encode('utf-8')+
-			str(self.blockNo).encode('utf-8'))
-		return h.hexdigest()
+		h = self.block_data().encode()
+		return hashlib.sha256(h).hexdigest()
 
 	def __str__(self):
-		return "Block Hash: " + str(self.hash()) + "\nBlockNo: " + str(self.blockNo) + "\nBlock Data: " + str(self.data) + "\nHashes: " + str(self.nonce) + "\n--------------"
+		return("Block Hash: " + str(self.hash())+ 
+			"\nindex: " + str(self.blockNo) + 
+			"\nBlock Data: " + str(self.block_data()) + 
+			"\nPoW: " + str(self.pow) + 
+			"\nPrevious Hash: " + str(self.previous_hash) + 
+			"\n--------------")
 
 
 class Blockchain:
 
 	diff = 20
-	maxNonce = 2**32
+	maxpow = 2**32
 	target = 2 ** (256-diff)
 
 	block = Block("Ziad")
@@ -44,18 +54,18 @@ class Blockchain:
 		self.block = self.block.next
 
 	def mine(self,block):
-		for n in range(self.maxNonce):
+		for n in range(self.maxpow):
 			if int(block.hash(), 16) <= self.target:
 				self.add(block)
 				print(block)
 				break
 			else:
-				block.nonce += 1
+				block.pow += 1
 
 
 blockchain = Blockchain()
 
-for n in range(20):
+for n in range(10):
 	blockchain.mine(Block("Block "+str(n+1)))
 
 while blockchain.head != None:
